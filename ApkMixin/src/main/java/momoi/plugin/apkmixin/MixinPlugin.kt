3999f4fdce6d5828970ec2daa89c7580b2979e58
huanli233 <392352840@qq.com>
2025-04-13 01:37:36 +0800

    refactor(ApkMixin): optimize the Mixin code structure, extract the code to MixinProcessor

--- ApkMixin/src/main/java/momoi/plugin/apkmixin/MixinPlugin.kt
+++ ApkMixin/src/main/java/momoi/plugin/apkmixin/MixinPlugin.kt
@@ -28,217 +28,104 @@ import java.io.FileNotFoundException
 import java.util.zip.ZipFile
 
 class MixinPlugin : Plugin<Project> {
-    // TODO 写得一坨 有机会一定要写漂亮一点
     override fun apply(project: Project) {
+        configureMixinTasks(project)
+    }
+
+    private fun configureMixinTasks(project: Project) {
+        createMixinBaseTask(project)
+        createMixinDebugTask(project)
+        createMixinReleaseTask(project)
+    }
+
+    private fun createMixinBaseTask(project: Project) {
         project.tasks.create("MixinApk") { task ->
             task.dependsOn("mergeDexRelease")
             task.doLast {
-                val srcDex = MultiDexIO.readDexFile(
-                    /* multiDex = */ true,
-                    /* file = */ File(
-                        project.projectDir,
-                        "build/intermediates/dex/release/mergeDexRelease"
-                    ),
-                    /* namer = */ BasicDexFileNamer(),
-                    /* opcodes = */ null,
-                    /* logger = */ null
-                )
-
-                val targetApkName = Config.targetApk ?: throw IllegalArgumentException("targetApk must not be null")
-                val targetApkFile = project.projectDir.child("mixin").child(targetApkName)
-                val trgDex = MultiDexIO.readDexFile(
-                    /* multiDex = */ true,
-                    /* file = */ targetApkFile,
-                    /* namer = */ BasicDexFileNamer(),
-                    /* opcodes = */ null,
-                    /* logger = */ null
-                )
-                val targetZipFile = ZipFile(targetApkFile)
-
-                val mixinClasses = mutableMapOf<ClassDef, ClassDef>()
-                srcDex.classes.forEach { srcDef ->
-                    if (srcDef.annotations.any { it.type == "Lmomoi/anno/mixin/Mixin;" }) {
-                        mixinClasses[srcDef] = srcDef.superclass?.let { s -> trgDex.findClass(s) }
-                            ?: throw FileNotFoundException(
-                                "Cannot find mixin ${srcDef.type} target class ${srcDef.superclass}"
-                            )
-                        info("Find Mixin Class: ${srcDef.type} to ${srcDef.superclass}")
-                    }
-                }
-
-                val newDex = MutableDexFile()
-                val changedDef = mutableMapOf<ClassDef, ClassDef>()
-                val modifiedClasses = mutableMapOf<String, ClassDef>()
-                srcDex.classes.forEach { srcDef ->
-                    var content = srcDef.toSmali()
-                    mixinClasses.forEach { (rs, rt) ->
-                        content = content.replace(rs.type, rt.type)
-                    }
-                    if (!mixinClasses.containsKey(srcDef)) {
-                        if (trgDex.findClass(srcDef.type) == null) {
-                            newDex.classes.add(content.toClassDef()!!)
-                        }
-                    } else {
-                        val rawTrgDef = mixinClasses[srcDef]!!
-                        val trgDef = changedDef[rawTrgDef] ?: rawTrgDef
-                        val srcSmali = Smali(content)
-                        val trgSmali = Smali(trgDef.toSmali())
-                        srcSmali.methods.forEach { srcMethod ->
-                            if (srcMethod.body.contains("Lmomoi/anno/mixin/StaticHook;")) {
-                                srcMethod.modifiers.add("static")
-                                if (srcMethod.name.endsWith("_")) {
-                                    srcMethod.name = srcMethod.name.removeSuffix("_")
-                                } else throw IllegalArgumentException("StaticHook method must end with '_': ${srcMethod.name}")
-                            }
-                            trgSmali.findMethod(srcMethod)?.let { trgMethod ->
-                                if (srcMethod.body.contains("Lmomoi/anno/mixin/PrivateCall;")) {
-                                    srcMethod.modifiers.remove("public")
-                                    srcMethod.modifiers.remove("protected")
-                                    srcMethod.modifiers.add("private")
-                                }
-                                srcMethod.body =
-                                    srcMethod.body.replace("invoke-super", "invoke-virtual")
-                                val rawCall = trgSmali.getMethodCall(trgMethod)
-                                var index = 0
-                                do {
-                                    trgMethod.name += "_${index++}"
-                                } while (trgSmali.methods.filter { it.name == trgMethod.name }.size > 1)
-                                srcMethod.body = srcMethod.body.replace(
-                                    rawCall,
-                                    trgSmali.getMethodCall(trgMethod)
-                                )
-                            }
-                            trgSmali.methods.add(srcMethod)
-                        }
-                        srcSmali.fields.forEach { srcField ->
-                            if (trgSmali.fields.find { it.name == srcField.name } == null) {
-                                trgSmali.fields.add(srcField)
-                            }
-                        }
-                        val newTrgDef = trgSmali.toText().toClassDef()!!
-                        changedDef[trgDef] = newTrgDef
-                        modifiedClasses[newTrgDef.type] = newTrgDef
-                    }
-                }
-
-                val rewriter = DexRewriter(
-                    object : RewriterModule() {
-                        override fun getDexFileRewriter(rewriters: Rewriters): Rewriter<DexFile?> {
-                            return object : DexFileRewriter(rewriters) {
-                                override fun rewrite(value: DexFile): DexFile {
-                                    return super.rewrite(
-                                        ImmutableDexFile(
-                                            value.opcodes,
-                                            buildList {
-                                                addAll(value.classes)
-
-                                                val types = modifiedClasses.keys
-                                                removeAll { it.type in types }
-
-                                                addAll(modifiedClasses.values)
-                                            }
-                                        )
-                                    )
-                                }
-                            }
-                        }
-                    }
-                )
-                info("Saving dex...")
-                val namer = BasicDexFileNamer()
-                val outputDexDir = project.projectDir.child("build/mixinDex")
-                MultiDexIO.writeDexFile(
-                    /* multiDex = */ true,
-                    /* threadCount = */ targetZipFile.getDexCount(namer),
-                    /* file = */ project.projectDir.child("build/mixinDex"),
-                    /* namer = */ namer,
-                    /* dexFile = */ rewriter.dexFileRewriter.rewrite(trgDex),
-                    /* maxDexPoolSize = */ DexIO.DEFAULT_MAX_DEX_POOL_SIZE,
-                    /* logger = */ null
-                )
-                info("Zip to mixin.apk...")
-                ZipUtil.addOrReplaceFilesInZip(
-                    project.projectDir.child("dist/mixin.apk"),
-                    outputDexDir.listFiles()?.associateBy { it.name } ?: emptyMap()
-                )
+                val processor = MixinProcessor(project)
+                processor.processMixin()
                 createMetadata(project)
             }
         }
+    }
+
+    private fun createMixinDebugTask(project: Project) {
         project.tasks.create("MixinAPK-debug") {
             it.dependsOn("MixinApk")
             it.doLast {
-                ManifestEditorMain.main(
-                    project.projectDir.child("dist/mixin.apk").absolutePath,
-                    "-o",
-                    project.projectDir.child("dist/unsign.apk").absolutePath,
-                    "-d", "1",
-                    "-vn", Config.versionName,
-                    "--force"
-                )
+                processManifest(project, isDebug = true)
                 sign(project)
             }
         }
+    }
+
+    private fun createMixinReleaseTask(project: Project) {
         project.tasks.create("MixinAPK-release") {
             it.dependsOn("MixinApk")
             it.doLast {
-                ManifestEditorMain.main(
-                    project.projectDir.child("dist/mixin.apk").absolutePath,
-                    "-o",
-                    project.projectDir.child("dist/unsign.apk").absolutePath,
-                    "-d", "0",
-                    "-vn", Config.versionName,
-                    "--force"
-                )
+                processManifest(project, isDebug = false)
                 sign(project)
             }
         }
     }
 
-    fun createMetadata(project: Project) {
-        project.projectDir.child("build/intermediates/apk_ide_redirect_file/debug/createDebugApkListingFileRedirect/redirect.txt").writeText(
-            """
-                #- File Locator -
-                listingFile=../../../../../dist/output-metadata.json
-            """.trimIndent()
-        )
-
-        project.projectDir.child("dist/output-metadata.json").writeText(
-            """
-                {
-                  "version": 3,
-                  "artifactType": {
-                    "type": "APK",
-                    "kind": "Directory"
-                  },
-                  "applicationId": "com.skywear.keyuploader",
-                  "variantName": "debug",
-                  "elements": [
-                    {
-                      "type": "SINGLE",
-                      "filters": [],
-                      "attributes": [],
-                      "outputFile": "mixin.apk"
-                    }
-                  ],
-                  "elementType": "File",
-                  "minSdkVersionForDexing": 24
-                }
-            """.trimIndent()
+    private fun processManifest(project: Project, isDebug: Boolean) {
+        ManifestEditorMain.main(
+            project.projectDir.child("dist/mixin.apk").absolutePath,
+            "-o",
+            project.projectDir.child("dist/unsign.apk").absolutePath,
+            "-d", if (isDebug) "1" else "0",
+            "-vn", Config.versionName,
+            "--force"
         )
     }
 
-    fun sign(project: Project) {
+    private fun createMetadata(project: Project) {
+        createRedirectFile(project)
+        createOutputMetadataFile(project)
+    }
+
+    private fun createRedirectFile(project: Project) {
+        project.projectDir.child("build/intermediates/apk_ide_redirect_file/debug/createDebugApkListingFileRedirect/redirect.txt")
+            .writeText("""
+                #- File Locator -
+                listingFile=../../../../../dist/output-metadata.json
+            """.trimIndent())
+    }
+
+    private fun createOutputMetadataFile(project: Project) {
+        project.projectDir.child("dist/output-metadata.json").writeText("""
+            {
+              "version": 3,
+              "artifactType": {
+                "type": "APK",
+                "kind": "Directory"
+              },
+              "applicationId": "com.skywear.keyuploader",
+              "variantName": "debug",
+              "elements": [
+                {
+                  "type": "SINGLE",
+                  "filters": [],
+                  "attributes": [],
+                  "outputFile": "mixin.apk"
+                }
+              ],
+              "elementType": "File",
+              "minSdkVersionForDexing": 24
+            }
+        """.trimIndent())
+    }
+
+    private fun sign(project: Project) {
         ApkSignerTool.main(arrayOf(
             "sign",
-            "--key",
-            project.projectDir.child("dist/testkey.pk8").absolutePath,
-            "--cert",
-            project.projectDir.child("dist/testkey.x509.pem").absolutePath,
-            "--out",
-            project.projectDir.child("dist/signed.apk").absolutePath,
+            "--key", project.projectDir.child("dist/testkey.pk8").absolutePath,
+            "--cert", project.projectDir.child("dist/testkey.x509.pem").absolutePath,
+            "--out", project.projectDir.child("dist/signed.apk").absolutePath,
             project.projectDir.child("dist/unsign.apk").absolutePath
         ))
         project.projectDir.child("dist/unsign.apk").delete()
     }
-
 }
+
---
+++ ApkMixin/src/main/java/momoi/plugin/apkmixin/MixinProcessor.kt
@@ -0,0 +1,207 @@
+package momoi.plugin.apkmixin
+
+import com.android.tools.smali.dexlib2.iface.ClassDef
+import com.android.tools.smali.dexlib2.iface.DexFile
+import com.android.tools.smali.dexlib2.immutable.ImmutableDexFile
+import com.android.tools.smali.dexlib2.rewriter.DexFileRewriter
+import com.android.tools.smali.dexlib2.rewriter.DexRewriter
+import com.android.tools.smali.dexlib2.rewriter.Rewriter
+import com.android.tools.smali.dexlib2.rewriter.RewriterModule
+import com.android.tools.smali.dexlib2.rewriter.Rewriters
+import lanchon.multidexlib2.BasicDexFileNamer
+import lanchon.multidexlib2.DexIO
+import lanchon.multidexlib2.MultiDexIO
+import momoi.plugin.apkmixin.utils.Smali
+import momoi.plugin.apkmixin.utils.SmaliMethod
+import momoi.plugin.apkmixin.utils.ZipUtil
+import momoi.plugin.apkmixin.utils.child
+import momoi.plugin.apkmixin.utils.findClass
+import momoi.plugin.apkmixin.utils.getDexCount
+import momoi.plugin.apkmixin.utils.info
+import momoi.plugin.apkmixin.utils.toClassDef
+import momoi.plugin.apkmixin.utils.toSmali
+import org.gradle.api.Project
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.zip.ZipFile
+
+class MixinProcessor(private val project: Project) {
+    fun processMixin() {
+        val (srcDex, trgDex, targetZipFile) = loadDexFiles()
+        val mixinClasses = findMixinClasses(srcDex, trgDex)
+        val modifiedClasses = processClasses(srcDex, trgDex, mixinClasses)
+        writeDexOutput(trgDex, modifiedClasses, targetZipFile)
+    }
+
+    private fun loadDexFiles(): Triple<DexFile, DexFile, ZipFile> {
+        val srcDex = MultiDexIO.readDexFile(
+            /* multiDex = */ true,
+            /* file = */ File(project.projectDir, "build/intermediates/dex/release/mergeDexRelease"),
+            /* namer = */ BasicDexFileNamer(),
+            /* opcodes = */ null,
+            /* logger = */ null
+        )
+
+        val targetApkName = Config.targetApk ?: throw IllegalArgumentException("targetApk must not be null")
+        val targetApkFile = project.projectDir.child("mixin").child(targetApkName)
+        val trgDex = MultiDexIO.readDexFile(
+            /* multiDex = */ true,
+            /* file = */ targetApkFile,
+            /* namer = */ BasicDexFileNamer(),
+            /* opcodes = */ null,
+            /* logger = */ null
+        )
+        val targetZipFile = ZipFile(targetApkFile)
+
+        return Triple(srcDex, trgDex, targetZipFile)
+    }
+
+    private fun findMixinClasses(srcDex: DexFile, trgDex: DexFile): Map<ClassDef, ClassDef> {
+        val mixinClasses = mutableMapOf<ClassDef, ClassDef>()
+        srcDex.classes.forEach { srcDef ->
+            if (srcDef.annotations.any { it.type == "Lmomoi/anno/mixin/Mixin;" }) {
+                mixinClasses[srcDef] = srcDef.superclass?.let { s -> trgDex.findClass(s) }
+                    ?: throw FileNotFoundException(
+                        "Cannot find mixin ${srcDef.type} target class ${srcDef.superclass}"
+                    )
+                info("Find Mixin Class: ${srcDef.type} to ${srcDef.superclass}")
+            }
+        }
+        return mixinClasses
+    }
+
+    private fun processClasses(
+        srcDex: DexFile,
+        trgDex: DexFile,
+        mixinClasses: Map<ClassDef, ClassDef>
+    ): Map<String, ClassDef> {
+        val newDex = MutableDexFile()
+        val changedDef = mutableMapOf<ClassDef, ClassDef>()
+        val modifiedClasses = mutableMapOf<String, ClassDef>()
+
+        srcDex.classes.forEach { srcDef ->
+            var content = srcDef.toSmali()
+            mixinClasses.forEach { (rs, rt) ->
+                content = content.replace(rs.type, rt.type)
+            }
+
+            when {
+                !mixinClasses.containsKey(srcDef) -> {
+                    if (trgDex.findClass(srcDef.type) == null) {
+                        newDex.classes.add(content.toClassDef()!!)
+                    }
+                }
+                else -> mixinToTargetClass(srcDef, content, mixinClasses, changedDef, modifiedClasses)
+            }
+        }
+
+        return modifiedClasses
+    }
+
+    private fun mixinToTargetClass(
+        srcDef: ClassDef,
+        content: String,
+        mixinClasses: Map<ClassDef, ClassDef>,
+        changedDef: MutableMap<ClassDef, ClassDef>,
+        modifiedClasses: MutableMap<String, ClassDef>
+    ) {
+        val rawTrgDef = mixinClasses[srcDef]!!
+        val trgDef = changedDef[rawTrgDef] ?: rawTrgDef
+        val srcSmali = Smali(content)
+        val trgSmali = Smali(trgDef.toSmali())
+
+        processMethods(srcSmali, trgSmali)
+        processFields(srcSmali, trgSmali)
+
+        val newTrgDef = trgSmali.toText().toClassDef()!!
+        changedDef[trgDef] = newTrgDef
+        modifiedClasses[newTrgDef.type] = newTrgDef
+    }
+
+    private fun processMethods(srcSmali: Smali, trgSmali: Smali) {
+        srcSmali.methods.forEach { srcMethod ->
+            processMethodStaticHook(srcMethod)
+            trgSmali.findMethod(srcMethod)?.let { trgMethod ->
+                processMethodBody(srcMethod, trgMethod, trgSmali)
+            }
+            trgSmali.methods.add(srcMethod)
+        }
+    }
+
+    private fun processMethodStaticHook(srcMethod: SmaliMethod) {
+        if (srcMethod.body.contains("Lmomoi/anno/mixin/StaticHook;")) {
+            srcMethod.modifiers.add("static")
+            if (srcMethod.name.endsWith("_")) {
+                srcMethod.name = srcMethod.name.removeSuffix("_")
+            } else throw IllegalArgumentException("StaticHook method must end with '_': ${srcMethod.name}")
+        }
+    }
+
+    private fun processMethodBody(srcMethod: SmaliMethod, trgMethod: SmaliMethod, trgSmali: Smali) {
+        if (srcMethod.body.contains("Lmomoi/anno/mixin/PrivateCall;")) {
+            srcMethod.modifiers.remove("public")
+            srcMethod.modifiers.remove("protected")
+            srcMethod.modifiers.add("private")
+        }
+        srcMethod.body = srcMethod.body.replace("invoke-super", "invoke-virtual")
+        val rawCall = trgSmali.getMethodCall(trgMethod)
+        var index = 0
+        do {
+            trgMethod.name += "_${index++}"
+        } while (trgSmali.methods.filter { it.name == trgMethod.name }.size > 1)
+        srcMethod.body = srcMethod.body.replace(rawCall, trgSmali.getMethodCall(trgMethod))
+    }
+
+    private fun processFields(srcSmali: Smali, trgSmali: Smali) {
+        srcSmali.fields.forEach { srcField ->
+            if (!trgSmali.fields.any { it.name == srcField.name }) {
+                trgSmali.fields.add(srcField)
+            }
+        }
+    }
+
+    private fun writeDexOutput(trgDex: DexFile, modifiedClasses: Map<String, ClassDef>, targetZipFile: ZipFile) {
+        val rewriter = createDexRewriter(modifiedClasses)
+        info("Saving dex...")
+        val namer = BasicDexFileNamer()
+        val outputDexDir = project.projectDir.child("build/mixinDex")
+        
+        MultiDexIO.writeDexFile(
+            /* multiDex = */ true,
+            /* threadCount = */ targetZipFile.getDexCount(namer),
+            /* file = */ outputDexDir,
+            /* namer = */ namer,
+            /* dexFile = */ rewriter.dexFileRewriter.rewrite(trgDex),
+            /* maxDexPoolSize = */ DexIO.DEFAULT_MAX_DEX_POOL_SIZE,
+            /* logger = */ null
+        )
+
+        info("Zip to mixin.apk...")
+        ZipUtil.addOrReplaceFilesInZip(
+            project.projectDir.child("dist/mixin.apk"),
+            outputDexDir.listFiles()?.associateBy { it.name } ?: emptyMap()
+        )
+    }
+
+    private fun createDexRewriter(modifiedClasses: Map<String, ClassDef>): DexRewriter {
+        return DexRewriter(object : RewriterModule() {
+            override fun getDexFileRewriter(rewriters: Rewriters): Rewriter<DexFile?> {
+                return object : DexFileRewriter(rewriters) {
+                    override fun rewrite(value: DexFile): DexFile {
+                        return super.rewrite(
+                            ImmutableDexFile(
+                                value.opcodes,
+                                buildList {
+                                    addAll(value.classes)
+                                    val types = modifiedClasses.keys
+                                    removeAll { it.type in types }
+                                    addAll(modifiedClasses.values)
+                                }
+                            )
+                        )
+                    }
+                }
+            }
+        })
+    }
+}
