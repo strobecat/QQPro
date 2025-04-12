3473bfcc0b75c050d340f305d10142fe51f6921d
dudu <duyuao200849@163.com>
2025-04-12 17:31:42 +0800

    滚轮适配可选择使用smoothScrollBy

--- app/src/main/java/momoi/mod/qqpro/Settings.kt
+++ app/src/main/java/momoi/mod/qqpro/Settings.kt
@@ -4,4 +4,5 @@ object Settings {
     val sp = Utils.application.getSharedPreferences("qqpro", 0)
     const val VERSION_CODE = 1
     val scale = sp.getFloat("scale", 0.9f)
+    val enableSmoothScroll = sp.getBoolean("enableSmoothScroll", false)
 }
