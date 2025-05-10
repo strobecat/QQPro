02552a2b606be23aa1d4277644d5ea6fb0c4e0ea
java30433 <2159116373@qq.com>
2025-05-10 17:47:38 +0800

    feat: 调整长按菜单按钮顺序
    Signed-off-by: java30433 <2159116373@qq.com>

---
+++ app/src/main/java/androidx/appcompat/widget/AppCompatTextView.java
@@ -0,0 +1,10 @@
+package androidx.appcompat.widget;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class AppCompatTextView extends TextView {
+    public AppCompatTextView(Context context) {
+        super(context);
+    }
+}
