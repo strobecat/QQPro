ccfa5b4128d30bc0302f3e3517e972329ea4e6c2
java30433 <2159116373@qq.com>
2025-05-10 23:14:55 +0800

    feat: 移动长按菜单选项

---
+++ app/src/main/java/momoi/mod/qqpro/hook/style/长按菜单调整.kt
@@ -0,0 +1,82 @@
+package momoi.mod.qqpro.hook.style
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.forEach
+import com.tencent.watch.aio_impl.ui.menu.AIOLongClickMenuFragment
+import momoi.anno.mixin.Mixin
+import momoi.mod.qqpro.Utils
+import momoi.mod.qqpro.asGroup
+import momoi.mod.qqpro.forEachAll
+import momoi.mod.qqpro.lib.FILL
+import momoi.mod.qqpro.lib.LinearScope
+import momoi.mod.qqpro.lib.background
+import momoi.mod.qqpro.lib.create
+import momoi.mod.qqpro.lib.dp
+import momoi.mod.qqpro.lib.height
+import momoi.mod.qqpro.lib.width
+
+val menuSort = arrayOf(
+    "回复",
+    "@Ta",
+    "复制文本",
+    "复读文本",
+    "去聊天",
+    "加好友",
+    "删除",
+)
+private fun process(group: ViewGroup) {
+    group.removeViewAt(0)
+    val linear = group.getChildAt(0).asGroup()
+        .getChildAt(0).asGroup()
+        .getChildAt(0) as LinearLayout
+    linear.background(0x44_000000)
+    val items = mutableMapOf<String, View>()
+    linear.forEach { item ->
+        item.asGroup().forEachAll {
+            if (it is AppCompatTextView) {
+                items[it.text.toString()] = item
+            }
+        }
+    }
+    linear.removeAllViews()
+    if (Utils.isRoundScreen) {
+        LinearScope(linear).add<View>()
+            .width(FILL)
+            .height(54.dp)
+    }
+    menuSort.forEach {
+        items[it]?.let { item ->
+            linear.addView(item)
+        }
+    }
+    items.values.forEach {
+        if (it.parent == null) {
+            linear.addView(it, 0)
+        }
+    }
+    if (Utils.isRoundScreen) {
+        LinearScope(linear).add<View>()
+            .width(FILL)
+            .height(54.dp)
+    }
+}
+
+@Mixin
+class 长按菜单透明 : AIOLongClickMenuFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)?.apply {
+            this.asGroup().getChildAt(0).asGroup().let { group ->
+                process(group)
+            }
+        }
+    }
+}
