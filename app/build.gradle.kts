4ead3ea1c493e1fecd8eb7e68f475b509d810f99
strobecat <203321455+strobecat@users.noreply.github.com>
2025-07-20 14:31:31 +0800

    feat: android 4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tencent.qqlite"
-        minSdk = 21
+        minSdk = 19
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
